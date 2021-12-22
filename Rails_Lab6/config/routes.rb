4b33d696830b9ed9f5023540541da4490bd05a02
Tiholaz Andrey <wildvilewingg@gmail.com>
2021-12-22 15:01:53 +0200

    Fixed Lab6 and added auth from Lab7

--- Rails_Lab6/config/routes.rb
+++ Rails_Lab6/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   get '/', to: 'root#hello_world'
 
   get '/reports', to: 'posts#index'
--- Rails_Lab6/db/schema.rb
+++ Rails_Lab6/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_181958) do
+ActiveRecord::Schema.define(version: 2021_12_18_202358) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_name", default: "first_name"
+    t.string "last_name", default: "last_name", null: false
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   create_table "ports", force: :cascade do |t|
     t.string "title"
@@ -26,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_11_04_181958) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foregin_key "reports" "users"
 end
